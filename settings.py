5b457b7f339aa1cc8239c2dc2e93880e47a3e1f0
Nikolay <zadneprovskiywork@gmail.com>
2022-01-20 15:37:53 +0200

    change settings

--- settings.py
+++ settings.py
@@ -1,8 +1,8 @@
 import os
 
 ITEM_NAME = os.environ['ITEM_NAME']
-GOOD_THRESHOLD = os.environ['THRESHOLD']
-QUERY_FREQ_MINUTES = os.environ['FREQ_MIN']
+GOOD_THRESHOLD = int(os.environ['THRESHOLD'])
+QUERY_FREQ_MINUTES = int(os.environ['FREQ_MIN'])
 
 WEBSITE = 'avito.ru'
 CACHE_SIZE = 100 # hours
