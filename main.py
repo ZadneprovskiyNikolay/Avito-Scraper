cf157d8b6b88225a596691d64daff63a4bb35ab9
Nikolay <zadneprovskiywork@gmail.com>
2022-01-20 15:48:04 +0200

    make script

--- main.py
+++ main.py
@@ -13,76 +13,60 @@ from settings import ITEM_NAME, GOOD_THRESHOLD, QUERY_FREQ_MINUTES, WEBSITE, CAC
 
 item_name_encoded = requests.utils.quote(ITEM_NAME)
 
-recent_items = [] # Push queue during last 48 hours
+# %%
+items = []
 
-while True:
-    # %%
-    items = []
+max_pages = 1
+for page_num in range(1, max_pages + 1):
 
-    max_pages = 1
-    for page_num in range(1, max_pages + 1):
+    res = get(
+        f'https://www.avito.ru/sankt-peterburg/telefony/mobilnye_telefony/apple-ASgBAgICAkS0wA3OqzmwwQ2I_Dc?cd=1&p={page_num}&q={item_name_encoded}&s=104',
+        headers=headers
+    )
 
-        res = get(
-            f'https://www.avito.ru/sankt-peterburg/telefony/mobilnye_telefony/apple-ASgBAgICAkS0wA3OqzmwwQ2I_Dc?cd=1&p={page_num}&q={item_name_encoded}&s=104',
-            headers=headers
-        )
-
-        soup = BeautifulSoup(res.text)
+    soup = BeautifulSoup(res.text)
 
 
-        tree_items = soup.find_all('div', {'data-marker': 'item'})
-        page_items = []
+    tree_items = soup.find_all('div', {'data-marker': 'item'})
+    page_items = []
 
-        for item in tree_items:
-            title = item.find('h3', {'itemprop': 'name'}).text
-            href = item.find('a', {'data-marker': 'item-title'})['href']
-            href = f'{WEBSITE}{href}'
-            date = item.find('div', {'data-marker': 'item-date'}).text        
-            try:
-                price = int(item.find('span', {'class': 'price-text-_YGDY'}).text[:-2].replace('\xa0', ''))
-            except:
-                continue
-            
-            page_items.append(Item(title=title, href=href, price=price, date_str=date))
+    for item in tree_items:
+        title = item.find('h3', {'itemprop': 'name'}).text
+        href = item.find('a', {'data-marker': 'item-title'})['href']
+        href = f'{WEBSITE}{href}'
+        date = item.find('div', {'data-marker': 'item-date'}).text        
+        try:
+            price = int(item.find('span', {'class': 'price-text-_YGDY'}).text[:-2].replace('\xa0', ''))
+        except:
+            continue
+        
+        page_items.append(Item(title=title, href=href, price=price, date_str=date))
 
-        # Отсеиваю неподходящие
-        page_items = list(filter(lambda item: (ITEM_NAME in item.title), page_items))
+    # Отсеиваю неподходящие
+    page_items = list(filter(lambda item: (ITEM_NAME in item.title), page_items))
 
-        if not page_items:
-            print(f'page {page_num} aborting, response: {soup}')
-            print()
-            break
+    if not page_items:
+        print(f'page {page_num} aborting, response: {soup}')
+        print()
+        break
 
-        items.extend(page_items)
+    items.extend(page_items)
 
-        print(f'page {page_num}: {len(page_items)} items ({len(items)} total)')
+    print(f'page {page_num}: {len(page_items)} items ({len(items)} total)')
 
-    # %%
-    items = list(set(items))
-    print(f'total: {len(items)}')
+# %%
+items = list(set(items))
+print(f'total: {len(items)}')
 
-    good = list(filter(lambda item: item.price <= GOOD_THRESHOLD, items))
-    good = sorted(good, key=lambda x: (x.time, x.price))
-    print(f'good: {len(good)}')
+good = list(filter(lambda item: item.price <= GOOD_THRESHOLD and item.time <= QUERY_FREQ_MINUTES, items))
+good = sorted(good, key=lambda x: (x.time, x.price))
+print(f'good: {len(good)}')
 
-    print(len(good))    
+print(len(good))    
 
-    for item in good:
-
-        # Check if item is already processed.
-        if item not in recent_items:
-
-            # Push item.
-            urllib.request.urlopen(f'http://pushmebot.ru/send?key=c74bd2b6021a3f352e5c08762fb53b1e&message={item.href}')
-
-            recent_items.append(item)
-            if len(recent_items) > CACHE_SIZE:
-                recent_items.pop(0)
-    
-
-        print(f'sleeping for {QUERY_FREQ_MINUTES} min...')
-        sleep(QUERY_FREQ_MINUTES * 60)
-
+for item in good:
+        # Push item.
+        urllib.request.urlopen(f'http://pushmebot.ru/send?key=c74bd2b6021a3f352e5c08762fb53b1e&message={item.href}')
 
 # %%
 
--- settings.py
+++ settings.py
@@ -5,4 +5,3 @@ GOOD_THRESHOLD = int(os.environ['THRESHOLD'])
 QUERY_FREQ_MINUTES = int(os.environ['FREQ_MIN'])
 
 WEBSITE = 'avito.ru'
-CACHE_SIZE = 100 # hours
