bef8d569a4206d2920d651f0954da2cc78f3fb16
Nikolay <zadneprovskiywork@gmail.com>
2022-02-25 16:43:45 +0300

    Phone screen settings

--- settings.py
+++ settings.py
@@ -1,7 +1,11 @@
 import os
 
-ITEM_NAME = os.environ['ITEM_NAME']
-GOOD_THRESHOLD = int(os.environ['THRESHOLD'])
-QUERY_FREQ_MINUTES = int(os.environ['FREQ_MIN'])
+# ITEM_NAME = os.environ['ITEM_NAME']
+# GOOD_THRESHOLD = int(os.environ['THRESHOLD'])
+# QUERY_FREQ_MINUTES = int(os.environ['FREQ_MIN'])
+
+ITEM_NAME = 'Google Pixel XL'
+GOOD_THRESHOLD = 4000
+QUERY_FREQ_MINUTES = 10000
 
 WEBSITE = 'avito.ru'
